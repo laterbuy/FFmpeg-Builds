6aae3753759e774cd561e1067bbbf3eca0f988b4
laterbuy <houbb1123@163.com>
2025-06-12 19:55:31 +0800

    Update 50-snappy.sh 禁用 启用 Snappy 压缩库

--- scripts.d/50-snappy.sh
+++ scripts.d/50-snappy.sh
@@ -4,7 +4,7 @@ SCRIPT_REPO="https://github.com/google/snappy.git"
 SCRIPT_COMMIT="6af9287fbdb913f0794d0148c6aa43b58e63c8e3"
 
 ffbuild_enabled() {
-    return 0
+    return -1
 }
 
 ffbuild_dockerbuild() {
