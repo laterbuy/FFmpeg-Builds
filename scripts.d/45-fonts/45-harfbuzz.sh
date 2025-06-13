8e456839147c5e577957fe9a95f91850030c8830
laterbuy <houbb1123@163.com>
2025-06-13 15:37:26 +0800

    Update 45-harfbuzz.sh 禁用字体库相关

--- scripts.d/45-fonts/45-harfbuzz.sh
+++ scripts.d/45-fonts/45-harfbuzz.sh
@@ -4,7 +4,7 @@ SCRIPT_REPO="https://github.com/harfbuzz/harfbuzz.git"
 SCRIPT_COMMIT="cd16559c278732cd4671921d35bfda27378f628a"
 
 ffbuild_enabled() {
-    return 0
+    return -1
 }
 
 ffbuild_dockerbuild() {
