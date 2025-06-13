537bde3b4c06970fb1c1fa14fcab554948b2368a
laterbuy <houbb1123@163.com>
2025-06-13 10:49:09 +0800

    Update 50-libzmq.sh 禁用 ZeroMQ（一个高性能异步消息库）实现 进程间通信（IPC） 或 网络流传输

--- scripts.d/50-libzmq.sh
+++ scripts.d/50-libzmq.sh
@@ -4,7 +4,7 @@ SCRIPT_REPO="https://github.com/zeromq/libzmq.git"
 SCRIPT_COMMIT="c2f7f8ae12720f48e97dc7d719da73cdab77e450"
 
 ffbuild_enabled() {
-    return 0
+    return -1
 }
 
 ffbuild_dockerbuild() {
