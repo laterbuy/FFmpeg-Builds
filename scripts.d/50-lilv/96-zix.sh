b23a984b786558ad83b4987cefd9da581716946c
laterbuy <houbb1123@163.com>
2025-06-13 16:39:03 +0800

    Update 96-zix.sh

--- scripts.d/50-lilv/96-zix.sh
+++ scripts.d/50-lilv/96-zix.sh
@@ -4,7 +4,7 @@ SCRIPT_REPO="https://github.com/drobilla/zix.git"
 SCRIPT_COMMIT="c271dbeb9787b8a223c3049342468e720cc760b3"
 
 ffbuild_enabled() {
-    return 0
+    return -1
 }
 
 ffbuild_dockerbuild() {
