e64098a7f3c0cf8f4622d71998403becd3386bf5
laterbuy <houbb1123@163.com>
2025-06-13 16:39:27 +0800

    Update 98-sratom.sh

--- scripts.d/50-lilv/98-sratom.sh
+++ scripts.d/50-lilv/98-sratom.sh
@@ -4,7 +4,7 @@ SCRIPT_REPO="https://github.com/lv2/sratom.git"
 SCRIPT_COMMIT="41f14e51def0a562d556a950d8cc36f15e480f7b"
 
 ffbuild_enabled() {
-    return 0
+    return -1
 }
 
 ffbuild_dockerbuild() {
