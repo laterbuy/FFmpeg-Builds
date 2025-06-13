1874f780afbb2d13150f0dfa8c6b978cc438c77d
laterbuy <houbb1123@163.com>
2025-06-13 11:01:42 +0800

    Update 50-gme.sh 禁用通常是指一个用于播放各种游戏机音乐格式的库，支持如 NSF、SPC、VGM 等格式

--- scripts.d/50-gme.sh
+++ scripts.d/50-gme.sh
@@ -4,7 +4,7 @@ SCRIPT_REPO="https://github.com/libgme/game-music-emu.git"
 SCRIPT_COMMIT="9762cbcff3d2224ee0f8b8c41ec143e956ebad56"
 
 ffbuild_enabled() {
-    return 0
+    return -1
 }
 
 ffbuild_dockerbuild() {
