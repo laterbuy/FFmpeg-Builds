5942a58f0505c236d69e5351728f6a9154b86b31
laterbuy <houbb1123@163.com>
2025-06-21 10:57:21 +0800

    Update defaults-gpl-shared.sh

--- variants/defaults-gpl-shared.sh
+++ variants/defaults-gpl-shared.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source "$(dirname "$BASH_SOURCE")"/defaults-gpl.sh
-FF_CONFIGURE+=" --disable-everything --enable-shared --disable-static --enable-encoder=h264_* --enable-libx264 --enable-encoder=libx264 --enable-swscale --enable-pthreads --enable-libvpl --enable-amf"
+FF_CONFIGURE+=" --disable-everything --enable-shared --disable-static --enable-encoder=h264_* --enable-libx264 --enable-encoder=libx264 --enable-swscale --enable-pthreads --enable-libvpl --enable-amf --enable-encoder=aac"
